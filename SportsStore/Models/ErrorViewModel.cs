d97262b908bd1e1fa5c96c76a9b1742a9b245f00
tmscheiber <thomas.m.scheiber@gmail.com>
2018-03-04 19:44:49 -0500

    Revert

--- SportsStore/Models/ErrorViewModel.cs
+++ SportsStore/Models/ErrorViewModel.cs
@@ -5,7 +5,6 @@ namespace SportsStore.Models
     public class ErrorViewModel
     {
         public string RequestId { get; set; }
-        public string dummy1 { get; set;  }
 
         public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
     }
