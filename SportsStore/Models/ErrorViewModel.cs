85bfc2b28f36567784088e0879f3536488271c28
tmscheiber <thomas.m.scheiber@gmail.com>
2018-03-04 19:38:56 -0500

    Test Commit

--- SportsStore/Models/ErrorViewModel.cs
+++ SportsStore/Models/ErrorViewModel.cs
@@ -5,6 +5,7 @@ namespace SportsStore.Models
     public class ErrorViewModel
     {
         public string RequestId { get; set; }
+        public string dummy1 { get; set;  }
 
         public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
     }
