2601c4d999c587fe50b2a8e8d8a7994adcfe1c5b
tmscheiber <thomas.m.scheiber@gmail.com>
2018-03-07 05:34:08 -0500

    TMS specific sports store

---
+++ TMSSportsStore/Models/Product.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace TMSSportsStore.Models
+{
+    public class Product
+    {
+        public int ProductID { get; set; }
+        public sgtring Name { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public string Category { get; set; }
+
+        public Product()
+        {
+        }
+    }
+}
---
+++ TMSSportsStoreTest/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace TMSSportsStoreTest
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
