c8783c3f68631be26686ff17b38a9953f5b5d7ea
tmscheiber <thomas.m.scheiber@gmail.com>
2018-03-14 21:56:24 -0400

    test

--- TMSSportsStore/Models/ViewModels/PagingInfo.cs
+++ TMSSportsStore/Models/ViewModels/PagingInfo.cs
@@ -11,5 +11,6 @@ namespace TMSSportsStore.Models.ViewModels
         public int CurrentPage { get; set; }
 
         public int TotalPages => (int)Math.Ceiling((decimal)TotalItems / ItemsPerPage);
+        //
     }
 }
