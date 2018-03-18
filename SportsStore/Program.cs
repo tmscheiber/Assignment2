f3be783feddb3e1acf2225fcadc3158d3c66aeab
tmscheiber <thomas.m.scheiber@gmail.com>
2018-03-18 18:55:26 -0400

    Finally deployed

--- SportsStore/Program.cs
+++ SportsStore/Program.cs
@@ -8,7 +8,6 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
-
 namespace SportsStore
 {
     public class Program
@@ -20,7 +19,7 @@ namespace SportsStore
 
         public static IWebHost BuildWebHost(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>()
+                   .UseStartup<Startup>().UseDefaultServiceProvider(options => options.ValidateScopes = false)
                 .Build();
     }
 }
